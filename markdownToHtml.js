c6c7bcb6aaa48a5f09043e241cd5b745b76f60d7
James Bowers <james@vizzly.co>
2022-09-06 07:16:46 -0700

    setup basic styles

--- markdownToHtml.js
+++
@@ -1,8 +0,0 @@
-import {remark} from 'remark'
-import html from 'remark-html'
-
-export const markdownToHtml = async (markdown) => {
-  const result = await remark().use(html).process(markdown)
-
-  return result.toString()
-}
